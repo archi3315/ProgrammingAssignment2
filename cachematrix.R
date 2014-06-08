db858b15bb6acad66b9a83a26e4dff1ba5e7914a
archi <aarchimowicz@gmail.com>
2014-06-07 22:46:27 -0300

    Implementation of makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are util functions to cache the inverse matrix calculation 
+## of an invertible matrix
 
-## Write a short comment describing this function
+## Auxiliar function to create a cacheable matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(originalmatrix = matrix()) {
+        solvedinverse <- NULL
+        set <- function(data) {
+                originalmatrix <<- data
+                solvedinverse <<- NULL
+        }
+        get <- function() {
+                ## Returns the original matrix
+                originalmatrix       
+        }
+        setinverse <- function(inverse) {
+                ## Sets the inversed matrix
+                solvedinverse <<- inverse       
+        }
+        getinverse <- function() {
+                ## Gets the inversed matrix
+                solvedinverse       
+        }
+        ## Returns the list of functions
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Calculates the inversed matrix of the given cachematrix
+## It returns the cached inversed matrix if exists
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cachematrix) {
+        ## Check for a cached solution
+        solvedinverse <- cachematrix$getinverse()
+        if(!is.null(solvedinverse)) {
+                message("getting cached inversed matrix")
+                return(solvedinverse)
+        }
+        
+        ## No cached solution. Solve and put it in the cache
+        data <- cachematrix$get()
+        solvedinverse <- solve(data)
+        cachematrix$setinverse(solvedinverse)
+        ## Return the solved inverse matrix of 'cachematrix'
+        solvedinverse
 }
